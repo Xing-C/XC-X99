71f131c313092f8c73faed10470afcae7d34473e
星尘 <39401415+Xing-C@users.noreply.github.com>
2024-10-12 23:35:07 +0800

    Delete mounriver_project/KEYBOARD_CH582M/obj directory

--- mounriver_project/KEYBOARD_CH582M/obj/Profile/scanparamservice.d
+++
@@ -1,7 +0,0 @@
-Profile/scanparamservice.o: ../Profile/scanparamservice.c \
- E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\HAL\include/CONFIG.h \
- E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\LIB/CH58xBLE_LIB.H \
- E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\Profile\include/scanparamservice.h
-E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\HAL\include/CONFIG.h:
-E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\LIB/CH58xBLE_LIB.H:
-E:\Personal\Personal\GitHub\XC-X99\mounriver_project\KEYBOARD_CH582M\Profile\include/scanparamservice.h:
